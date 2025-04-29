da78ba2ffdb00176aa97feb35469b7755f9e9abc
Godwin <godwin7gj@gmail.com>
2025-04-29 21:39:53 +0530

    Experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Skills from './components/Pages/Homepage/Skills/skills';
 import Education from './components/Pages/Homepage/Education/Education';
+//import Experience from './components/Pages/Homepage/Experience/Experience';
 
 function App() {
   return (
     <div className="bg-[#0d1224] min-h-screen text-white">
       <Skills />
       <Education/>
+      {/*<Experience/>*/}
     </div>
   );
 }
