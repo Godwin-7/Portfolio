a20c1c319767eb6d9683fd44d0beeb2ecb62d4e0
Godwin <godwin7gj@gmail.com>
2025-07-17 01:31:32 +0530

    Modified Contact colours

--- src/components/Pages/Homepage/Footer/Footer.jsx
+++ src/components/Pages/Homepage/Footer/Footer.jsx
@@ -1,60 +1,34 @@
 import './Footer.css'
 import reactIcon from './svg/react-icon.svg'
-import linkedin from './svg/linkedin.svg'
-import githubIcon from './svg/github.svg'
-import whatsapp from './svg/whatsapp.svg'
-import telegram from './svg/telegram.svg'
-import instagramIcon from './svg/instagram.svg'
 
 export function Footer() {
+  const currentYear = new Date().getFullYear()
+  const currentDateTime = new Date().toLocaleString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  })
+
   return (
     <footer className="footer">
-      <a href="https://vinayaksingh.in" className="logo">
-        <span>www.Godwin</span>
-        <span>A.in</span>
-      </a>
-      <div>
-        <p>
-          This Website was made with <img src={reactIcon} alt="React" />
-          {/* <span>❤️</span> */}
-        </p>
-      </div>
-      <div className="social-media">
-        <a
-          href="https://www.linkedin.com/in/codevinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={linkedin} alt="Linkedin" />
+      <div className="footer-content">
+        <a href="https://vinayaksingh.in" className="logo">
+          <span>www.Godwin</span>
+          <span>A.in</span>
         </a>
-        <a
-          href="https://github.com/CodeVinayak/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={githubIcon} alt="GitHub" />
-        </a>
-        <a
-          href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={whatsapp} alt="Whatsapp" />
-        </a>
-        <a
-          href="https://t.me/CodeVinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={telegram} alt="telegram" />
-        </a>
-        <a
-          href="https://www.instagram.com/vinayaksingh.in"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={instagramIcon} alt="Instagram" />
-        </a>
+        <div className="made-with">
+          <p>
+            This Website was made with <img src={reactIcon} alt="React" />
+          </p>
+        </div>
+        <div className="copyright-info">
+          <p className="copyright">&copy; {currentYear} Godwin A. All rights reserved.</p>
+          <p className="datetime">{currentDateTime}</p>
+        </div>
       </div>
     </footer>
   )
