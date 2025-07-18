9cd1dff9b5bb6ba8867f7d1696fa8e99369f363d
Godwin <godwin7gj@gmail.com>
2025-07-19 00:28:28 +0530

    Modified Home section

--- src/components/Pages/Homepage/Home/Home.jsx
+++ src/components/Pages/Homepage/Home/Home.jsx
@@ -37,57 +37,57 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <section className="home" id="home">
-        <div className="home-content">
-          <h3>Hi, I am</h3>
-          <h1>Godwin Wilfred</h1>
-          <p></p><br />
-          <p>Aspiring Data Scientist & Machine Learning Enthusiast</p>
-          
-          <div className="home-social-media">
-            <a href="https://www.linkedin.com/in/ganesharam2003/" target="_blank" rel="noopener noreferrer">
-              <i className='bx bxl-linkedin'></i>
-            </a>
-            <a href="https://github.com/ganesha62" target="_blank" rel="noopener noreferrer">
-              <i className='bx bxl-github'></i>
-            </a>
-            <a href="https://www.instagram.com/g_raam62/?hl=en" target="_blank" rel="noopener noreferrer">
-              <i className='bx bxl-instagram'></i>
-            </a>
+    <section className="home" id="home">
+      <div className="home-container">
+        <div className="home-row">
+          <div className="home-content-column">
+            <div className="home-content">
+              <h3>Hi, I am</h3>
+              <h1>Godwin Wilfred</h1>
+              <p>Aspiring Data Scientist &<br />Machine Learning Enthusiast</p>
+              
+              <div className="home-social-media">
+                <a href="https://www.linkedin.com/in/ganesharam2003/" target="_blank" rel="noopener noreferrer">
+                  <i className='bx bxl-linkedin'></i>
+                </a>
+                <a href="https://github.com/ganesha62" target="_blank" rel="noopener noreferrer">
+                  <i className='bx bxl-github'></i>
+                </a>
+                <a href="https://www.instagram.com/g_raam62/?hl=en" target="_blank" rel="noopener noreferrer">
+                  <i className='bx bxl-instagram'></i>
+                </a>
+              </div>
+              <a href="#contact" id="contact_button" className="home-btn">Contact Me</a>
+            </div>
           </div>
-          <a href="#contact" id="contact_button" className="home-btn">Contact Me</a>
-        </div>
 
-        <div className="home-profession-container">
-          <div className="home-profession-box">
-            <div className="home-profession" style={{'--i': 0}}>
-              <i className='bx bx-code-alt'></i>
-              <h3>Aspiring Data Scientist</h3>
+          <div className="home-animation-column">
+            <div className="home-profession-container">
+              <div className="home-profession-box">
+                <div className="home-profession" style={{'--i': 0}}>
+                  <i className='bx bx-code-alt'></i>
+                  <h3>Data Scientist</h3>
+                </div>
+                <div className="home-profession" style={{'--i': 1}}>
+                  <i className='bx bx-book-heart'></i>
+                  <h3>Web Developer</h3>
+                </div>
+                <div className="home-profession" style={{'--i': 2}}>
+                  <i className='bx bxs-graduation'></i>
+                  <h3>ML Enthusiast</h3>
+                </div>
+                <div className="home-profession" style={{'--i': 3}}>
+                  <i className='bx bxl-react'></i>
+                  <h3>AI Engineer</h3>
+                </div>
+                <div className="home-circle"></div>
+              </div>
+              <div className="home-overlay"></div>
             </div>
-            <div className="home-profession" style={{'--i': 1}}>
-              <i className='bx bx-book-heart'></i>
-              <h3>Web Developer</h3>
-            </div>
-            <div className="home-profession" style={{'--i': 2}}>
-              <i className='bx bxs-graduation'></i>
-              <h3>ML Enthusiast</h3>
-            </div>
-            <div className="home-profession" style={{'--i': 3}}>
-              <i className='bx bxl-react'></i>
-              <h3>Aspiring AI Engineer</h3>
-            </div>
-            <div className="home-circle"></div>
           </div>
-          <div className="home-overlay"></div>
         </div>
-        
-        <div className="home-img">
-          {/* Image commented out for custom animation */}
-          {/* <img src="home.png" alt="Photo" /> */}
-        </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
